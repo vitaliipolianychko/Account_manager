fa03806cede7a72d005557d59999a66b032d27ee
vitaliipolianychko <vet.polyanychko@gmail.com>
2019-10-05 16:31:41 +0300

    Updates

---
+++ precache-manifest.1c64f9051e8124ddfecc8c9f3138d917.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "4b1ff10e10a008c98a88721319693b95",
+    "url": "/Account_manager/index.html"
+  },
+  {
+    "revision": "63521a904dd636999e72",
+    "url": "/Account_manager/static/css/main.3640f1d8.chunk.css"
+  },
+  {
+    "revision": "c159b98616a6dad10c05",
+    "url": "/Account_manager/static/js/2.8aeef31e.chunk.js"
+  },
+  {
+    "revision": "63521a904dd636999e72",
+    "url": "/Account_manager/static/js/main.5b718c19.chunk.js"
+  },
+  {
+    "revision": "0e6f20ecda13cb1c8613",
+    "url": "/Account_manager/static/js/runtime-main.15b059d9.js"
+  },
+  {
+    "revision": "2551f83e1b7e1d156dd8d5392c89e697",
+    "url": "/Account_manager/static/media/Logo.2551f83e.svg"
+  },
+  {
+    "revision": "7e9d623db05c8aa46ec4b34453653574",
+    "url": "/Account_manager/static/media/addUsers.7e9d623d.svg"
+  },
+  {
+    "revision": "f41d10ed448a258f32b9a9aac4e683ba",
+    "url": "/Account_manager/static/media/listOfUsers.f41d10ed.svg"
+  }
+]);
